bd6e4cfc771d6d99936746cc2f1a586f8a48d563
Abhilash George <abhygate@gmail.com>
2026-09-12 10:07:37 +0530

    Add EC2 AMI lookup and network configuration

---
+++ terraform/network.tf
@@ -0,0 +1,28 @@
+data "aws_vpc" "default" {
+  default = true
+}
+resource "aws_security_group" "app_sg" {
+  name        = "cloud-native-devops-app-sg"
+  description = "Allow HTTP access to Spring Boot application"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "Spring Boot application"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "cloud-native-devops-app-sg"
+  }
+}
