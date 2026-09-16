0c5ced85e7703b4ad57fd44e04ef2a538c84106e
Abhilash George <abhygate@gmail.com>
2026-09-16 11:19:52 +0530

    Add SSM access to EC2 IAM role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -22,8 +22,13 @@ resource "aws_iam_role_policy_attachment" "ecr_read_only" {
   role       = aws_iam_role.ec2_ecr_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "cloud-native-devops-ec2-profile"
   role = aws_iam_role.ec2_ecr_role.name
 }
+
